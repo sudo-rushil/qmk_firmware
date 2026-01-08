7f503fd26e3bf6bbd270232d80b7a4701f878dac
Rushil Mallarapu <rushil.mallarapu@pm.me>
2026-01-08 23:12:32 +0800

    feat: make left top thumb LCmd for use with macos
    in future, might need to hard reset the board with pins to put in
    bootloader mode...

--- keyboards/keebio/iris_ce/keymaps/iris_rushil/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/iris_rushil/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TL_UPPR,          KC_RGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSET,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,          KC_RGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSET,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LCTL, KC_LALT, KC_ENT,                    KC_SPC,  TL_LOWR, TL_UPPR
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
